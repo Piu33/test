d04dfcd55e190208067844644e074d46b78b3ca3
SalvatoreFerrara <140163275+Piu33@users.noreply.github.com>
2025-02-10 12:09:58 +0100

    fix

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # Leggi il file Excel, pandas potrebbe già interpretare la colonna come datetime
-df = pd.read_excel("data.xlsx")
+df = pd.read_excel("files/data.xlsx")
 
 # Converte la colonna della data in stringa e poi applica il formato desiderato
 df["DATA"] = pd.to_datetime(df["DATA"]).dt.strftime("%d.%m.%Y")
 
 # Salva in JSON
-df.to_json("data.json", orient="records", indent=4, force_ascii=False)
+df.to_json("files/data.json", orient="records", indent=4, force_ascii=False)
 
 print("Dati salvati in data.json")
