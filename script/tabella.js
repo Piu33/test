fb5ca8f5c98e80660520a0b294b2a623df54b1e5
SalvatoreFerrara <140163275+Piu33@users.noreply.github.com>
2025-02-10 12:50:15 +0100

    add table

--- script/tabella.js
+++ script/tabella.js
@@ -1,17 +1,27 @@
 async function caricaDati() {
-  const response = await fetch('data.json');
+  const response = await fetch('files/data.json');
   const dati = await response.json();
   
-  let table = "<table border='1'><tr>";
+  let table = "<table><thead><tr>";
   for (let key in dati[0]) {
       table += `<th>${key}</th>`;
   }
-  table += "</tr>";
+  table += "</tr></thead>";
   
   dati.forEach(row => {
       table += "<tr>";
       for (let key in row) {
+        if (row[key] == "BOT" || row[key] == "SOLD"){
+          if (row[key] == "BOT"){
+            table += `<td style="color: blue">${row[key]}</td>`
+          }
+          if (row[key] == "SOLD"){
+            table += `<td style="color:red">${row[key]}</td>`
+          }
+        }
+        else {
           table += `<td>${row[key]}</td>`;
+        }
       }
       table += "</tr>";
   });
